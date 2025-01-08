ab5e173ca6eeb166fe72e878d3d93a1240434c44
sonia1115 <soniareactdeveloper@gmail.com>
2025-01-09 03:10:38 +0600

    nodemon and express installed

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
-const http = require("http");
+// const http = require("http");
 
-http.createServer((req, res) => {
-  res.write("hello")
-  res.end();
+// http.createServer((req, res) => {
+//   res.write("hello world");
+//   res.end();
 
-}).listen(9000);
+// }).listen(9000);
 
 // to stop a running server write ctrl+c
+
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+})
+
+app.listen(9000, () => {
+  console.log("Server is running on port 9000");
+})
