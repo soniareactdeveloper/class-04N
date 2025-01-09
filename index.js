cb3ffd9bf762efdb26071e2c1beeb05dee09c707
sonia1115 <soniareactdeveloper@gmail.com>
2025-01-10 01:40:57 +0600

    post method

--- index.js
+++ index.js
@@ -1,19 +1,28 @@
-// const http = require("http");
-
-// http.createServer((req, res) => {
-//   res.write("hello world");
-//   res.end();
-
-// }).listen(9000);
-
 // to stop a running server write ctrl+c
 
 const express = require("express");
+const user = require("./user");
+
+
+
+
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("Hello World");
+})
+
+app.get("/users", (req, res) => {
+  res.send(user);
+})
+
+
+app.post ("/login", (req, res) => {
+   const { name, age } = req.body;
+   console.log(age);
 })
 
 app.listen(9000, () => {
